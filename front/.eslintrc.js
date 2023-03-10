fb0e821d6785991ee3b562200b8859d88ccec6cf
moriyama yukyo <moriyama-yukyo@alfos.jp>
2023-03-10 05:45:58 +0000

    自動フォーマットするように設定

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,32 +1,29 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'next/core-web-vitals',
-    'google',
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "next/core-web-vitals",
+    "google",
+    "prettier",
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  'plugins': [
-    'react',
-    'react-hooks',
-    '@typescript-eslint',
-  ],
-  'rules': {
-    'semi': ['error', 'never'],
-    'require-jsdoc': ['off'],
-    'spaced-comment': ['off'],
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  rules: {
+    semi: ["error", "never"],
+    "require-jsdoc": ["off"],
+    "spaced-comment": ["off"],
   },
-}
+};
