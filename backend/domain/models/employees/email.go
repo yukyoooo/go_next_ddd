f118da236db03a34a72c2d8f29471a5eb1ef4f71
moriyama yukyo <moriyama-yukyo@alfos.jp>
2023-03-13 02:12:02 +0000

    add email 値obj

---
+++ backend/domain/models/employees/email.go
@@ -0,0 +1,36 @@
+package employee
+
+import (
+	"fmt"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/yukyoooo/go_next_ddd/iterrors"
+)
+
+type Email struct {
+	value string
+}
+
+func (e Email) Value() string {
+	return e.value
+}
+
+func NewEmail(value string) (_ *Email, err error) {
+	defer iterrors.Wrap(&err, "email.NewEmail(%s", value)
+	email := new(Email)
+
+	if value == "" {
+		return nil, fmt.Errorf("email is required")
+	}
+
+	if !validateEmail(value) {
+		return nil, fmt.Errorf("wrong format")
+	}
+	email.value = value
+
+	return email, nil
+}
+
+func validateEmail(value string) bool {
+	return govalidator.IsEmail(value)
+}
