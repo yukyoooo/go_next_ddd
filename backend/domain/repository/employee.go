ba880cc9ae3af99fb3dbf1f2345fc3626c88a49c
moriyama yukyo <moriyama-yukyo@alfos.jp>
2023-03-15 00:54:19 +0000

    add employee repository

---
+++ backend/domain/repository/employee.go
@@ -0,0 +1,14 @@
+package repository
+
+import employee "github.com/yukyoooo/go_next_ddd/domain/model/employee"
+
+type EmployeeRepository interface {
+	
+	save(first_name string, last_name string, email string, password string, role int) (error)
+
+	getById(id int) (*employee.Employee, error)
+
+	update(id int, first_name string, last_name string, email string, password string, role int) (error)
+
+	delete(id int) (error)
+}
