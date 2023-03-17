f1b45a14cfe94e8c19b1b857571171bfd85a5cb8
moriyama yukyo <moriyama-yukyo@alfos.jp>
2023-03-17 13:18:06 +0000

    add error var

--- backend/ierrors/ierrors.go
+++ backend/ierrors/ierrors.go
@@ -9,3 +9,6 @@ func Wrap(errp *error, format string, args ...interface{}) {
 		*errp = fmt.Errorf("%s: %w", fmt.Sprintf(format, args...), *errp)
 	}
 }
+
+var ErrInvalidName = fmt.Errorf("名前は必須です")
+var ErrInvalidDate = fmt.Errorf("日付形式が間違っています")
