a95f013c92023f56a2ec8d4a74c6f46c50d80726
moriyama yukyo <moriyama-yukyo@alfos.jp>
2023-03-23 00:34:38 +0000

    add status enum

--- backend/enum/role.go
+++ backend/enum/role.go
@@ -3,28 +3,6 @@ package enum
 type Role int
 
 const (
-	Waiting Role = iota
-	Working
-	ReviewRequest
-	InReview
-	completed
+	PG Role = iota
+	SE
 )
-
-/*
-func (r Role) String() string {
-	switch r {
-	case Waiting:
-		return "未着手"
-	case Working:
-		return "作業中"
-	case ReviewRequest:
-		return "レビュー依頼中"
-	case InReview:
-		return "レビュー中"
-	case completed:
-		return "完了"
-	default:
-		return "未定義の役割"
-	}
-}
-*/
