d137510075ae3450ee71d88ff7b69fa19b6297fc
moriyama yukyo <moriyama-yukyo@alfos.jp>
2023-04-07 14:37:38 +0900

    add storybook

---
+++ frontend/.storybook/main.ts
@@ -0,0 +1,25 @@
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
+import path from 'path'
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-postcss'],
+  staticDirs: ['public'],
+  babel: async (options) => ({
+    ...options,
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+    ],
+  }),
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+      }),
+    ]
+
+    return config
+  },
+}
---
+++ frontend/.storybook/preview.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { theme } from '../src/themes'
+import * as NextImage from 'next/image'
+
+
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+export const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  textarea {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    transition: .25s;
+    color: #000000;
+  }
+`
+
